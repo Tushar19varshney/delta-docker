6691be42b3a4c7ca018cfc111edb44dc7cdc9a33
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-05-17 11:42:32 +0530

    feat: bash file for updating repositories

---
+++ pull.sh
@@ -0,0 +1,25 @@
+
+# Switching to delta-frontend
+cd delta-frontend
+
+# Switching to development branch
+git checkout development
+
+# Pulling from upstream repository in development branch
+git pull https://github.com/shreyanshjn/delta-frontend.git development
+
+# Switching to previous directory
+cd ..
+
+# Switching to delta-backend
+cd delta-backend
+
+# Switching to development branch
+git checkout development
+
+# Pulling from upstream repository
+git pull https://github.com/Tushar19varshney/delta-backend.git development
+
+# Switching to previous directory
+cd ..
+printf "All repositories updated successfully\n"
